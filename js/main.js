d4591d8a60e5d4126f085a0826433b6cebb18464
vaibhav goyal <vgoyal20@iitk.ac.in>
2022-12-17 12:02:37 +0530

    added mobile styles

--- js/main.js
+++ js/main.js
@@ -181,24 +181,24 @@ dotsContainer.addEventListener("click", function (e) {
     }
   });
 
-  window.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const scrollY = window.pageYOffset;
+  // window.addEventListener("scroll", () => {
+  //   const sections = document.querySelectorAll("section");
+  //   const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-      let sectionHeight = current.offsetHeight;
-      let sectionTop = current.offsetTop - 50;
-      let id = current.getAttribute("id");
+  //   sections.forEach((current) => {
+  //     let sectionHeight = current.offsetHeight;
+  //     let sectionTop = current.offsetTop - 50;
+  //     let id = current.getAttribute("id");
 
-      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-        document
-          .querySelector(".navlinks ul li a[href*=" + id + "]")
-          .classList.add("active");
-      } else {
-        document
-          .querySelector(".navlinks ul li a[href*=" + id + "]")
-          .classList.remove("active");
-      }
-    });
-  });
+  //     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+  //       document
+  //         .querySelector(".navlinks ul li a[href*=" + id + "]")
+  //         .classList.add("active");
+  //     } else {
+  //       document
+  //         .querySelector(".navlinks ul li a[href*=" + id + "]")
+  //         .classList.remove("active");
+  //     }
+  //   });
+  // });
 })(jQuery);
