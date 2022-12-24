2e47c8e2909bb366f9f3d3572167447f06780d2e
vaibhav goyal <vgoyal20@iitk.ac.in>
2022-12-24 21:14:23 +0530

    changed contact details

--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,26 @@
-ScrollReveal({
-    repeat: "false",
-    distance: "8px",
-    delay: 100,
-  });
-  ScrollReveal().reveal(".heading", {
-    delay: 200,
-    distance: "15px",
-    origin: "left",
-  });
-  ScrollReveal().reveal(".para",{
-    delay: 300,
-    distance: "20px",
-    origin:"right",
-  });
-  ScrollReveal().reveal(".eve-heading",{delay:300});
-  ScrollReveal().reveal(".eve-info",{delay:400});
-  ScrollReveal().reveal(".eve-card-ani",{delay:500, distance:"100px", origin:"left"});
-  ScrollReveal().reveal(".eve-card-ani2",{delay:500, distance:"100px", origin:"right"});
-  ScrollReveal().reveal(".comp-info",{delay:400});
-  ScrollReveal().reveal(".comp-card",{delay:700});
-  ScrollReveal().reveal(".ws-card",{delay:700});
-  ScrollReveal().reveal(".ws-heading",{delay:400});
-  ScrollReveal().reveal(".ws-desc",{delay:600});
-  ScrollReveal().reveal(".sp-heading",{delay:600});
-  ScrollReveal().reveal(".sp-desc",{delay:800});
+// ScrollReveal({
+//     repeat: "false",
+//     distance: "8px",
+//     delay: 100,
+//   });
+//   ScrollReveal().reveal(".heading", {
+//     delay: 200,
+//     distance: "15px",
+//     origin: "left",
+//   });
+//   ScrollReveal().reveal(".para",{
+//     delay: 300,
+//     distance: "20px",
+//     origin:"right",
+//   });
+//   ScrollReveal().reveal(".eve-heading",{delay:300});
+//   ScrollReveal().reveal(".eve-info",{delay:400});
+//   ScrollReveal().reveal(".eve-card-ani",{delay:500, distance:"100px", origin:"left"});
+//   ScrollReveal().reveal(".eve-card-ani2",{delay:500, distance:"100px", origin:"right"});
+//   ScrollReveal().reveal(".comp-info",{delay:400});
+//   ScrollReveal().reveal(".comp-card",{delay:700});
+//   ScrollReveal().reveal(".ws-card",{delay:700});
+//   ScrollReveal().reveal(".ws-heading",{delay:400});
+//   ScrollReveal().reveal(".ws-desc",{delay:600});
+//   ScrollReveal().reveal(".sp-heading",{delay:600});
+//   ScrollReveal().reveal(".sp-desc",{delay:800});
